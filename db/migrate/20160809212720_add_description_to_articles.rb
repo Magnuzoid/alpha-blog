3ed485a68783842f68c5f5ab6557c08405e2fccf
magnuzoid <magnus.nielsen@gmail.com>
2016-08-10 12:26:17 +0000

    fix heroku migration issues

--- db/migrate/20160809212720_add_description_to_articles.rb
+++ db/migrate/20160809212720_add_description_to_articles.rb
@@ -1,6 +1,6 @@
 class AddDescriptionToArticles < ActiveRecord::Migration
   def change
-    add_column :articles, :description, :text
+    #add_column :articles, :description, :text
     add_column :articles, :created_at, :timedate
     add_column :articles, :updated_at, :timedate
   end
