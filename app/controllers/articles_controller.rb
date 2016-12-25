9a78c310fefe86336621f7af807e4ce19fddb41c
magnuzoid <magnus.nielsen@gmail.com>
2016-12-25 01:06:24 +0100

    update all views with categories

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,23 +2,23 @@ class ArticlesController < ApplicationController
     before_action :set_article, only: [:edit, :update, :show, :destroy]
     before_action :require_user, except: [:index, :show]
     before_action :require_same_user, only: [:edit, :update, :destroy]
-    
-    
+
+
     def index
-        @articles = Article.paginate(page: params[:page], per_page: 2)
+        @articles = Article.paginate(page: params[:page], per_page: 10)
     end
-    
-    
+
+
     def new
         @article = Article.new
     end
-    
+
     def edit
-        
+
     end
-    
+
     def update
-        
+
         if @article.update(article_params)
             flash[:success] = "Article was succesfully updated!"
             redirect_to article_path(@article)
@@ -26,7 +26,7 @@ class ArticlesController < ApplicationController
             render 'edit'
         end
     end
-    
+
     def create
         #render plain: params[:article].to_json
         #debugger
@@ -35,39 +35,39 @@ class ArticlesController < ApplicationController
         @article.user = current_user
         #@article.save
         #redirect_to article_path(@article)
-        
-        if @article.save 
+
+        if @article.save
             flash[:success] = "Article was succesfully added to database"
             redirect_to article_path(@article)
         else
             render 'new'
         end
     end
-    
+
     def show
-        
+
     end
-    
+
     def destroy
         @article.destroy
         flash[:danger] = "Article was succesfully removed"
         redirect_to articles_path
     end
-    
+
     private
         def set_article
             @article = Article.find(params[:id])
         end
-    
+
         def article_params
-            params.require(:article).permit(:title, :description)
+            params.require(:article).permit(:title, :description, category_ids: [])
         end
-        
+
         def require_same_user
             if current_user != @article.user and !current_user.admin?
                 flash[:danger] = "You can only edit or delete your own articles"
                 redirect_to root_path
             end
-            
+
         end
 end
